4ffb266cabcac5b5d9fc8fbbbf1ec168ec72adf5
HarshPatelSchool <harshpatelschool2021@gmail.com>
2022-03-18 15:42:04 -0400

    Added "Branch - Harsh" print

--- src/main/java/edu/wpi/eleven/Main.java
+++ src/main/java/edu/wpi/eleven/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
   public static void main(String[] args) {
     App.launch(App.class, args);
-    System.out.println("Ben Schmitt");
-    System.out.println("Harsh Patel");
+    System.out.println("Branch - Harsh");
   }
 }

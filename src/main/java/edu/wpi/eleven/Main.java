11aa52d823e34c24eeb736a5600566d431d00119
Tim-K3007 <77994355+Tim-K3007@users.noreply.github.com>
2022-03-18 15:51:32 -0400

    adding my name Tim

--- src/main/java/edu/wpi/eleven/Main.java
+++ src/main/java/edu/wpi/eleven/Main.java
@@ -4,15 +4,12 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("Ben Schmitt");
-<<<<<<< HEAD
     System.out.println("Edward Enyedy");
-=======
     System.out.println("Harsh Patel");
     System.out.println("Timothy Klein");
     System.out.println("Zhuofan Lu");
     System.out.println("Marko Vila");
     App.launch(App.class, args);
->>>>>>> a9e9d07c2c134fa0ca51ebb3f256d3502bc55ea5
   }
 }
 

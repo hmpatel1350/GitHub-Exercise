32ae0ced7d489e979f3bbb3305ba16ff49bfee14
HarshPatelSchool <harshpatelschool2021@gmail.com>
2022-03-18 15:39:49 -0400

    Added "Harsh Patel" print

--- src/main/java/edu/wpi/eleven/Main.java
+++ src/main/java/edu/wpi/eleven/Main.java
@@ -3,8 +3,8 @@ package edu.wpi.eleven;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Harsh Patel");
     App.launch(App.class, args);
     System.out.println("Ben Schmitt");
+    System.out.println("Harsh Patel");
   }
 }

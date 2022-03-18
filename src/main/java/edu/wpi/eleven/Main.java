a9e9d07c2c134fa0ca51ebb3f256d3502bc55ea5
HarshPatelSchool <harshpatelschool2021@gmail.com>
2022-03-18 15:47:59 -0400

    Moved App after prints so names are actually shown

--- src/main/java/edu/wpi/eleven/Main.java
+++ src/main/java/edu/wpi/eleven/Main.java
@@ -3,10 +3,10 @@ package edu.wpi.eleven;
 public class Main {
 
   public static void main(String[] args) {
-    App.launch(App.class, args);
     System.out.println("Ben Schmitt");
     System.out.println("Harsh Patel");
     System.out.println("Zhuofan Lu");
     System.out.println("Marko Vila");
+    App.launch(App.class, args);
   }
 }
